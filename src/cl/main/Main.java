1943d44d3582691173c5a5c4082e61e416c70707
Claudio <claudio.alarcon2298@gmail.com>
2022-08-07 19:46:58 -0400

    menu en el main

--- src/cl/main/Main.java
+++ src/cl/main/Main.java
@@ -1,10 +1,12 @@
 package cl.main;
-
+import java.io.IOException;
+import cl.vista.Menu;
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		Menu menu = new Menu();
+		menu.iniciarMenu();
 	}
 
 }
