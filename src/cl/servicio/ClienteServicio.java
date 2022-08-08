d74470e6693a50efcdc3755578ee59e745b142fb
Eddy <eddy.gallardo03@gmail.com>
2022-08-07 23:09:24 -0400

    Editar cliente funcionando

--- src/cl/servicio/ClienteServicio.java
+++ src/cl/servicio/ClienteServicio.java
@@ -3,15 +3,24 @@ package cl.servicio;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+import java.util.Scanner;
 import cl.modelo.CategoriaEnum;
 import cl.modelo.Cliente;
+import cl.utitlidades.Utilidad;
 
 
 public class ClienteServicio {
 	//creacion de lista
+	String respuesta;
+	int opcion=0;
+	int opcion2 = 0;
+		Scanner leer = new Scanner(System.in);
+		
+		Utilidad utilidad = new Utilidad();
 		
 		List<Cliente> listaClientes = new ArrayList<>();
+		
+		
 	//constructor 
 		public ClienteServicio(List<Cliente> listaClientes) {
 			super();
@@ -53,4 +62,139 @@ public class ClienteServicio {
 			listaClientes.add(modeloclientes);
 			listaClientes.add(modeloClienteNull);
 		} 
+		
+public void retornoEditarCliente()  {
+			
+			
+			System.out.println("Ingrese RUN del Cliente a editar:");
+			respuesta = leer.next();
+			
+			System.out.println(respuesta);
+			
+			Cliente cliente = buscarClientePorRun(respuesta);
+					
+
+			if (cliente == null) {
+
+				System.out.println("Rut no existe");
+			} else {
+					System.out.println("------blaaaa------");
+				
+				do {
+					System.out.println("Seleccione que desea hacer: ");
+					System.out.println("1.-Cambiar el estado del Cliente");
+					System.out.println("2.-Editar los datos ingresados del Cliente");
+					System.out.println("Ingrese opcion: ");
+					opcion = leer.nextInt();
+					switch (opcion) {
+					case 1:
+						System.out.println("------Actualizando estado del Cliente ");
+						System.out.println("El estado actual es: ");
+						System.out.println("¿Desea cambiar el estado?");
+						System.out.println("1 - Si");
+						System.out.println("2 - No");
+						System.out.println("Ingrese opcion: ");
+						opcion = leer.nextInt();
+						do {
+							switch(opcion){
+								case 1:
+									
+									if(cliente.getNombreCategoria() == CategoriaEnum.ACTIVO) {
+										cliente.setNombreCategoria(CategoriaEnum.INACTIVO);
+										System.out.println("Se cambio el estado correctamente a: "+cliente.getNombreCategoria());
+									}else {
+										cliente.setNombreCategoria(CategoriaEnum.ACTIVO);
+										
+									}
+									utilidad.tiempoEspera();
+									//menu.iniciarMenu();
+									break;
+								case 2:
+									System.out.println("Se mantuvo el estado actual del Cliente");
+									utilidad.tiempoEspera();
+									//menu.iniciarMenu();
+									break;
+								default:
+									utilidad.mostrarMensaje();
+									break;
+							}
+						}while(opcion <1 || opcion>2);
+						utilidad.tiempoEspera();
+						utilidad.limpieza();
+						break;
+					case 2:
+						System.out.println("------Actualizando estado del Cliente------");
+						System.out.println("1.- El RUN del Cliente es: " + cliente.getRunCliente() );
+						System.out.println("2.- El Nombre del Cliente es: "+ cliente.getNombreCliente());
+						System.out.println("3.- El Apellido del Cliente es: "+ cliente.getApellidoCliente());
+						System.out.println("4.- Los años del Cliente son: "+ cliente.getAniosCliente());
+						System.out.println("Ingrese opcion a editar: ");
+						opcion2 = leer.nextInt();
+						System.out.println("-------------------------------------------");
+						 do{
+							switch (opcion2) {
+							case 1:
+								System.out.println("1.- Ingrese nuevo RUN del Cliente: ");
+								respuesta = leer.next();
+								cliente.setRunCliente(respuesta);
+								System.out.println("Datos cambiados con éxito");
+								break;
+							case 2:
+								System.out.println("2.- Ingrese nuevo Nombre del Cliente: ");
+								respuesta = leer.next();
+								cliente.setNombreCliente(respuesta);
+								System.out.println("Datos cambiados con éxito");
+								break;
+							case 3:
+								System.out.println("3.- Ingrese nuevo Apellido del Cliente: ");
+								respuesta = leer.next();
+								cliente.setApellidoCliente(respuesta);
+								System.out.println("Datos cambiados con éxito");
+								break;
+							case 4:
+								System.out.println("4.- Ingrese nuevos años como Cliente: ");
+								respuesta = leer.next();
+								cliente.setAniosCliente(respuesta);
+								System.out.println("Datos cambiados con éxito");
+								utilidad.tiempoEspera();
+								//menu.iniciarMenu();
+								break;
+							default:
+								System.out.println("Opcion no valida");
+								break;
+							}
+						} while (opcion2 <1||opcion2>4);
+						 System.out.println("salir");
+						System.out.println("-------------------------------------------");
+						utilidad.limpieza();
+						utilidad.tiempoEspera();
+						
+						//menu.iniciarMenu();
+						break;
+					default:
+						System.out.println("Opcion no valida");
+						break;
+						
+					}
+				}while (opcion<1||opcion>2);
+			}
+
+			utilidad.tiempoEspera();
+			utilidad.limpieza();
+			
+			}
+		
+		
+		private Cliente buscarClientePorRun(String rutCliente) {// compara rut con arreglo
+
+		for (Cliente cliente : this.listaClientes) {
+				if (rutCliente.equals(cliente.getRunCliente())) { 
+			return cliente;
+				}
+			}
+
+		return null;
+		}
+		
+		
 }
